599f3ccf17690ae5e18914c6015a7cd50d1d2485
Francisco_Arguedas <farguedas99@gmail.com>
2022-08-31 13:49:18 -0600

    Methods and documentation added

--- dbConnection.js
+++ dbConnection.js
@@ -3,7 +3,7 @@ const oracledb = require("oracledb");
 // hr schema password
 
 var password = "system";
-
+var connection;
 // checkConnection asycn function
 
 async function checkConnection() {
@@ -13,38 +13,42 @@ async function checkConnection() {
 
       password: password,
 
-      connectString: "localhost:1521/xepdb1",
+      connectString: "localhost:1521",
     });
 
     console.log("connected to database");
 
-    /* result = await connection.execute(`
-    CREATE TABLE EMPLOYEES(
-        id int NOT NULL, 
-        first_name VARCHAR(45) NOT NULL,
-        last_name VARCHAR(45) NOT NULL,
-        PRIMARY KEY(id)
-        )`);*/
+    /******************************************************* */
 
-    var result = await connection.execute(`
-        INSERT INTO EMPLOYEES (id, first_name, last_name) VALUES (
-            505, 
-            'Franciscooo123',
-            'Arguedas'
-            )`);
+    //Reference: https://www.w3schools.com/sql/
+    //Play with these three methods (Implemented below, you can make changes)
 
+    //Uncomment them and see the outputs by opening a terminal where this files are located and put the command:
+    // node dbConnection.js
+
+    //Will create a table
+    // createTable();
+
+    //Will insert data into the table
+    //insertIntoTable();
+
+    //Will show the data inside the table
+    // selectAll();
+
+    //Will delete the table
+    //dropTable();
+
+    /******************************************************* */
+
+    //Will submit an persistently save every change made
     connection.commit();
-
-    result = await connection.execute(`SELECT * FROM EMPLOYEES`);
-    console.log(result);
-
-    //result = await connection.execute(`DROP TABLE EMPLOYEES`);
   } catch (err) {
+    //Connection error
     console.error(err.message);
   } finally {
     if (connection) {
       try {
-        // Always close connections
+        // Always close connections with the DB
 
         await connection.close();
 
@@ -53,6 +57,59 @@ async function checkConnection() {
         console.error(err.message);
       }
     }
+  }
+}
+
+async function createTable() {
+  try {
+    let result = await connection.execute(`
+    CREATE TABLE  employees(
+        id int NOT NULL, 
+        first_name VARCHAR(45) NOT NULL,
+        last_name VARCHAR(45) NOT NULL,
+        PRIMARY KEY(id)
+        )`);
+    console.log("Tabla creada correctamente.");
+  } catch (err) {
+    //ORA-00955: este nombre ya lo está utilizando otro objeto existente
+    //Will appear if the table already exists
+    console.error(err.message);
+  }
+}
+
+async function insertIntoTable() {
+  try {
+    let result = await connection.execute(`
+        INSERT INTO EMPLOYEES (id, first_name, last_name) VALUES (
+            505, 
+            'Francisco',
+            'Arguedas'
+            )`);
+    //Will save the changes
+
+    console.log("Datos añadidos a la tabla correctamente.");
+  } catch (err) {
+    //ORA-00001: restricción única (SYSTEM.SYS_C008586) violada
+    //Will appear if the ID of the inserted data is already in the table (PRIMARY KEY)
+    console.error(err.message);
+  }
+}
+
+async function selectAll() {
+  try {
+    result = await connection.execute(`SELECT * FROM EMPLOYEES`);
+    console.log(result);
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+async function dropTable() {
+  try {
+    result = await connection.execute(`DROP TABLE EMPLOYEES`);
+    console.log("Tabla eliminada correctamente.");
+  } catch (err) {
+    console.error("La tabla no existe.");
   }
 }
 
